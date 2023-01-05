bc6a4e88e1b55d96b67509e8cbb7cf160342e43b
Samandar <samandarebet@gmail.com>
2023-01-05 16:08:05 +0500

    Crud movie homework

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
 	"app/config"
+	"app/controller"
 	"app/pkg/db"
 )
 
@@ -13,28 +16,45 @@ func main() {
 
 	conn, err := db.NewConnectPostgres(cfg)
 	if err != nil {
-		log.Fatal("error databse connection", err.Error())
+		log.Fatal("error database connection: ", err.Error())
 	}
 
-	query := `
-		insert into movie (
-			id,
-			title,
-			duration,
-			description
-		) values ($1, $2, $3, $4)
-	`
+	cont := controller.NewController(conn)
 
-	_, err = conn.Exec(query,
-		"f904d5c9-3e24-4637-b0d7-b3d103d2011c",
-		"Avatar 2",
-		"03:00:00",
-		"Best movie",
-	)
+	http.HandleFunc("/movie", cont.Movie)
 
+	fmt.Println("Listening", cfg.HTTPPort)
+	err = http.ListenAndServe(cfg.HTTPPort, nil)
 	if err != nil {
-		log.Println("error whiling insert movie: ", err)
-		return
+		log.Fatal("error listening server: ", err.Error())
 	}
+
+	// cfg := config.Load()
+
+	// conn, err := db.NewConnectPostgres(cfg)
+	// if err != nil {
+	// 	log.Fatal("error databse connection", err.Error())
+	// }
+
+	// query := `
+	// 	insert into movie (
+	// 		id,
+	// 		title,
+	// 		duration,
+	// 		description
+	// 	) values ($1, $2, $3, $4)
+	// `
+
+	// _, err = conn.Exec(query,
+	// 	"f904d5c9-3e24-4637-b0d7-b3d103d2011c",
+	// 	"Avatar 2",
+	// 	"03:00:00",
+	// 	"Best movie",
+	// )
+
+	// if err != nil {
+	// 	log.Println("error whiling insert movie: ", err)
+	// 	return
+	// }
 
 }
--- storage/movie.go
+++ storage/movie.go
@@ -65,3 +65,121 @@ func GetByIdMovie(db *sql.DB, id string) (models.Movie, error) {
 
 	return movie, nil
 }
+
+func GetAllMovie(db *sql.DB) ([]models.Movie, error) {
+
+	var (
+		movie []models.Movie
+	)
+
+	query := `
+		SELECT
+			id,
+			title,
+			TO_CHAR(duration, 'HH24:MI:SS'),
+			description
+		FROM movie
+	`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return []models.Movie{}, err
+	}
+
+	for rows.Next() {
+		var m models.Movie
+
+		err = rows.Scan(
+			&m.Id,
+			&m.Title,
+			&m.Duration,
+			&m.Description,
+		)
+		if err != nil {
+			return []models.Movie{}, err
+		}
+
+		movie = append(movie, m)
+	}
+
+	return movie, nil
+}
+
+func Update(db *sql.DB, movie models.Movie) error {
+
+	query := `
+		UPDATE movie
+			set title=$2,
+			    duration=$3,
+			    description=$4
+		 WHERE id = $1
+	`
+
+	_, err := db.Exec(query,
+		movie.Id,
+		movie.Title,
+		movie.Duration,
+		movie.Description,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Delete(db *sql.DB, id string) error {
+
+	query := `
+		DELETE from movie
+		 WHERE id = $1
+	`
+
+	_, err := db.Exec(query, id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetListMovie(db *sql.DB) ([]models.Movie, error) {
+
+	var (
+		movie []models.Movie
+	)
+
+	query := `
+		SELECT
+			id,
+			title,
+			TO_CHAR(duration, 'HH24:MI:SS'),
+			description
+		FROM movie
+	`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		return []models.Movie{}, err
+	}
+
+	for rows.Next() {
+		var m models.Movie
+
+		err = rows.Scan(
+			&m.Id,
+			&m.Title,
+			&m.Duration,
+			&m.Description,
+		)
+		if err != nil {
+			return []models.Movie{}, err
+		}
+
+		movie = append(movie, m)
+	}
+
+	return movie, nil
+}
